73a1a779d6ed3a757abc9ec799d1c7044daddc06
ssial <ssial@uoguelph.ca>
2021-10-17 13:06:20 -0400

    removing extra comments from my loop.rb code

--- loop.rb
+++ loop.rb
@@ -1,3 +1,6 @@
+# Written by Sawaiba Sial
+# CIS3260
+
 class Loop
     def initialize(length, width, offset, boardSpaces)
         if offset==1
@@ -12,7 +15,6 @@ class Loop
     end
 
     def validcapture(collision, colour)
-        # colour2 = spaceAt(collision).getPiece().getColour()
         colour2 = :red
         if colour != colour2
             return true
@@ -27,72 +29,54 @@ class Loop
         fcollisions = Array.new
         bcollisions = Array.new
         testCoordHash = piece.spacePosition()
-        # colour = piece.getColour()
 
-        #print "TESTPIECE: #{piece.spacePosition()}\n"
         # Test forward/clockwise paths
         flc = firstLoopCheck(testCoordHash, true)
-        #print "\nFLC: #{flc}\n"
 
         if flc
-            #print "Testing Forward Paths\n"
             testedFLoops.push(flc)
             testCoord = @loop_exits[[flc,"F"]]
             testCoordHash = {"x" => testCoord[0].to_i, "y" => testCoord[1].to_i}
             fcollisions.push(testCoordHash)
             (1..4).each do
-                #print "forward(#{testCoordHash}, #{testedFLoops})\n"
                 result = forward(testCoordHash, testedFLoops)
-                ##print "result: #{result} \n"
                 if result
                     testedFLoops.push(result[0])
                     testCoord = @loop_exits[[result[0],"F"]]
                     testCoordHash = {"x" => testCoord[0].to_i, "y" => testCoord[1].to_i}
                     if result[1].kind_of?(Hash)
-                        #if validcapture(result[1],colour)
                             fcollisions.push(result[1])
-                        #end
                     end
                 end
             end
             testCoord = @loop_exits[[testedFLoops.last,"F"]]
             testCoordHash = {"x" => testCoord[0].to_i, "y" => testCoord[1].to_i}
             result = clearPath(testCoordHash, {"x" => testedFLoops.first[0].to_i, "y" => testedFLoops.first[1].to_i})
-            ##print "result: #{result}\n"
             if result.kind_of?(Hash)
-                #if validcapture(result,colour)
                     fcollisions.push(result)
-                #end
             end
         end
 
         # Test backward/anti-clockwise paths
-
         testedBLoops = Array.new
         testCoordHash = piece.spacePosition()
 
         flc = firstLoopCheck(testCoordHash, false)
-        #print "\nFLC: #{flc}\n"
+
 
         if flc
-            #print "Testing Backward Paths\n"
             testedBLoops.push(flc)
             testCoord = @loop_exits[[flc,"B"]]
             testCoordHash = {"x" => testCoord[0].to_i, "y" => testCoord[1].to_i}
             bcollisions.push(testCoordHash)
-            # colour = piece.getColour()
             (1..4).each do
-                ##print "backward(#{testCoordHash}, #{testedBLoops})\n"
                 result = backward(testCoordHash, testedBLoops)
-                ##print "result: #{result} \n"
                 if result
                     testedBLoops.push(result[0])
                     testCoord = @loop_exits[[result[0],"B"]]
                     testCoordHash = {"x" => testCoord[0].to_i, "y" => testCoord[1].to_i}
                     if result[1].kind_of?(Hash)
-                        #if validcapture(result[1],colour)
                             bcollisions.push(result[1])
-                        #end
                     end
                 end
             end
@@ -100,9 +84,7 @@ class Loop
             testCoordHash = {"x" => testCoord[0].to_i, "y" => testCoord[1].to_i}
             result = clearPath(testCoordHash, {"x" => testedBLoops.first[0].to_i, "y" => testedBLoops.first[1].to_i})
             if result.kind_of?(Hash)
-                #if validcapture(result,colour)
                     bcollisions.push(result)
-                #end
             end
         end
 
@@ -115,7 +97,7 @@ class Loop
         else
             collisions.push(bcollisions[0])
         end
-        #print "\n Collisions: #{collisions}\n"
+     
         return collisions
 
 
@@ -123,7 +105,6 @@ class Loop
 
     def spaceEmpty(coord)
         key = coord["x"].to_s + coord["y"].to_s
-        ##print "key: #{key}"
         if @boardSpaces[key].getPiece()
             return false
         end
@@ -131,7 +112,6 @@ class Loop
     end
 
     def firstLoopCheck(position, forward)
-        ##print "FLC: pos - #{position}, forward?#{forward}\n"
         if forward
             possible_exits = @loop_exits.keys.select{|e| e.include?"F"}
         else
@@ -144,17 +124,13 @@ class Loop
             coordsHash = {"x" => coords[0].to_i, "y" => coords[1].to_i}
             if (position["x"]-coordsHash["x"])==0 and (position["y"]-coordsHash["y"])==0
                 result = clearPath(position, coordsHash)
-                #print "SAME!comparing position: #{position} vs. coordsHash: #{coordsHash}\n results: #{result}\n"
                 if result and !result.kind_of?(Hash)
                     return coords
                 end
             elsif position["y"] - coordsHash["y"] == 0 and (position["y"]==0 or position["y"]==5)
-                #print "#{position} vs. #{coordsHash} --> first or last row\n"
             elsif position["x"] - coordsHash["x"] == 0 and (position["x"]==0 or position["x"]==5)
-                #print "#{position} vs. #{coordsHash} --> first or last col\n"
             else
                 result = clearPath(position, coordsHash)
-                #print "comparing position: #{position} vs. coordsHash: #{coordsHash}\n results: #{result}\n"
                 if result and !result.kind_of?(Hash)
                     return coords
                 end
@@ -165,17 +141,6 @@ class Loop
 
     # returns true if there's a clear path between two given points, false if there's no direct path, otherwise returns an array of pieces that block the direct path
     def clearPath(p1,p2)
-        ###print "\n comparing #{p1} and #{p2}\n"
-        # check if points are adjacent and diagonal
-        # if (p1["x"]-p2["x"]).abs()==1 and (p1["y"]-p2["y"]).abs()==1
-        #     return true
-        # check if points are equal to eachother
-
-
-
-
-
-
         if (p1["x"]-p2["x"])==0 and (p1["y"]-p2["y"])==0
             return true
         elsif (p1["x"]-p2["x"])==0 || (p1["y"]-p2["y"])==0
@@ -220,7 +185,6 @@ class Loop
                     if spaceEmpty(position)
                         counter+=1
                     else
-                        #collisions.push(position)
                         return position
                     end
                 else
@@ -228,7 +192,6 @@ class Loop
                     if spaceEmpty(position)
                         counter+=1
                     else
-                        # collisions.push(position)
                         return position
                     end
                 end
@@ -252,23 +215,20 @@ class Loop
             if !avoid.include?coords
                 coordsHash = {"x" => coords[0].to_i, "y" => coords[1].to_i}
                 if position["y"] - coordsHash["y"] == 0 and (position["y"]==0 or position["y"]==5)
-                    ##print "#{position} vs. #{coordsHash} --> first or last row\n"
                 elsif position["x"] - coordsHash["x"] == 0 and (position["x"]==0 or position["x"]==5)
-                    ##print "#{position} vs. #{coordsHash} --> first or last col\n"
                 else
                     result = clearPath(position, coordsHash)
                     if result
-                        ##print "\nRESULT: #{result}\n"
                         return coords, result
                     end
                 end
 
             end
         end
-        ##print "ENDDD #{coords}"
         return false
     end
 
+    #find nearest loop exit in counter-clockwise direction
     def backward(position, avoid)
         possible_exits = @loop_exits.keys.select{|e| e.include?"B"}
         coords = 0
@@ -278,20 +238,16 @@ class Loop
             if !avoid.include?coords
                 coordsHash = {"x" => coords[0].to_i, "y" => coords[1].to_i}
                 if position["y"] - coordsHash["y"] == 0 and (position["y"]==0 or position["y"]==5)
-                    ##print "#{position} vs. #{coordsHash} --> first or last row\n"
                 elsif position["x"] - coordsHash["x"] == 0 and (position["x"]==0 or position["x"]==5)
-                    ##print "#{position} vs. #{coordsHash} --> first or last col\n"
                 else
                     result = clearPath(position, coordsHash)
                     if result
-                        ##print "\nRESULT: #{result}\n"
                         return coords, result
                     end
                 end
 
             end
         end
-        ##print "ENDDD #{coords}"
         return false
     end
 
@@ -300,12 +256,3 @@ class Loop
     end
 
 end
-
-#innerloop = Loop.new(6,6,1, boardSpaces)
-# innerloop.getCollisions({"x" => 4, "y" => 3})
-
-# result = innerloop.clearPath({"x" => 3, "y" => 3},{"x" => 6, "y" => 2})
-# ##print "\nRESULT\n"
-# ##print result
-# ##print "\nisArray: #{result.kind_of?(Array)}\n"
-#innerloop.getCollisions({"x" => 4, "y" => 2})
